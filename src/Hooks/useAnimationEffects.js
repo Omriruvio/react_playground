0c076a25e87e6915685f6e790d62dfb58664e918
Omri Ruvio <omriruvio@gmail.com>
2022-11-18 23:13:27 +0200

    add animation effect custom hook

---
+++ src/Hooks/useAnimationEffects.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useMemo } from 'react';
+
+/**
+ * hook to return animation styles for a component
+ * if shouldAnimate is true, the component will animate from fromStyles to toStyles
+ * if no duration, timingFunction or delay is provided, the default values will be used
+ *
+ * @param {{ [key: string]: string }} fromStyles - css styles object for the element before animation
+ * @param {{ [key: string]: string }} toStyles - css styles object for the element after animation
+ * @param {boolean} shouldAnimate - boolean to determine if the animation should be played
+ * @param {number[]} durations - array of durations for each transition
+ * @param {string[]} timingFunctions - array of timing functions for each transition
+ * @param {number[]} delays - array of delays for each transition
+ * @returns {{ [key: string]: string }} - css styles object for the element to be animated
+ */
+const useAnimationStyles = (fromStyles, toStyles, shouldAnimate, durations, timingFunctions, delays) => {
+  const transitionString = useMemo(() => {
+    return Object.keys(fromStyles)
+      .filter((style) => style !== 'transition')
+      .reduce((acc, key, i) => {
+        const duration = `${durations[i]}s` || '0.5s';
+        const timingFunction = timingFunctions[i] || 'ease-in-out';
+        const delay = `${delays[i]}s` || '0s';
+        return `${acc ? `${acc}, ` : ''}${key} ${duration} ${timingFunction} ${delay}`;
+      }, '');
+  }, [durations, timingFunctions, delays, fromStyles]);
+
+  const [styles, setStyles] = useState(() =>
+    shouldAnimate ? { ...fromStyles, transition: transitionString } : { ...toStyles, transition: transitionString }
+  );
+
+  useEffect(() => {
+    if (shouldAnimate) {
+      setTimeout(() => {
+        setStyles({ ...toStyles, transition: transitionString });
+      }, 0);
+    }
+  }, [shouldAnimate, toStyles, transitionString]);
+
+  return styles;
+};
+
+export default useAnimationStyles;
